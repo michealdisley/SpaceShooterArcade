3e6de290426a553d7ca7634caa835f7354de924b
Micheal Disley <strdircorvinus@gmail.com>
2017-08-03 04:31:31 -0400

    Started on Build pipeline & downloaded SDK & JDK packs

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -1,22 +1,37 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class BuildScript {
-	static string[] devScenes = {  "Assets/Scenes/DevGame.unity" };
+public class BuildScript
+{
+
+    static string[] devScenes = new string[] {  "Assets/Scenes/DevGame.unity" };
 	static string[] relScenes = {  };
 
-	[MenuItem("Builds/OSX/Development")]
-	private static void BuildDevOSX() {
-		BuildOSX(true);
+    [MenuItem("Tools/Builds/OSX/Development")]
+    private static void BuildDevOSX()
+    {
+        BuildOSX(true);
+    }
+
+    [MenuItem("Tools/Builds/OSX/Release")]
+    private static void BuildRelOSX()
+    {
+        BuildOSX(false);
+    }
+
+    [MenuItem("Tools/Builds/Andriod/Development")]
+    private static void BuildDevAndriod()
+    {
+        BuildAndroid(true);
+    }
+
+    [MenuItem("Tools/Builds/Andriod/Release")]
+    private static void BuildRelAndroid()
+    {
+		BuildAndroid(false);
 	}
 
-
-	private static void BuildRelOSX() {
-		BuildOSX(false);
-	}
-
-
-	private static void BuildOSX(bool isDev) {
+    private static void BuildOSX(bool isDev) {
 		Debug.Log("Building OSX" + (isDev ? " Dev" : "") );
 		string buildPath =  GetBuildPath("OSX");
 
@@ -26,8 +41,21 @@ public class BuildScript {
 		BuildPipeline.BuildPlayer( isDev ? devScenes : relScenes, buildPath, BuildTarget.StandaloneOSXUniversal, isDev ? BuildOptions.Development : BuildOptions.None);
 	}
 
-	private static string GetBuildPath(string subDirectory) {
+    private static void BuildAndroid(bool isDev)
+    {
+        Debug.Log("Building Android" + (isDev ? " Dev" : ""));
+        string buildPath = GetBuildPath("Android");
+
+        if (isDev)
+            buildPath +=
+
+        BuildPipeline.BuildPlayer(isDev ? devScenes : relScenes, buildPath, BuildTarget.Android, isDev ? BuildOptions.Development : BuildOptions.None);
+    }
+
+    private static string GetBuildPath(string subDirectory)
+    {
         return Application.dataPath;
 	}
+
 
 }
